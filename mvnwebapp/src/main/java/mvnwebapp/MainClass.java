a61fc0f68336e4365cf429ed480e9b879ec64f08
RanganaGangadhar <35066304+RanganaGangadhar@users.noreply.github.com>
2018-01-04 11:44:20 +0530

    first commit

---
+++ mvnwebapp/src/main/java/mvnwebapp/MainClass.java
@@ -0,0 +1,13 @@
+package mvnwebapp;
+
+public class MainClass {
+
+	public MainClass() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("::::::mvn testing:::::;");
+	}
+
+}

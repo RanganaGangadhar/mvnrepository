97f6a2fa5f5a03b353a0faed77720a70b30f7fdf
Pravat Ranjan Pradhan <Pravat.Pradhan@marlabs.com>
2018-01-16 15:34:08 +0530

    'added'

---
+++ mvnwebapp/src/main/java/mvnwebapp/Ranjan.java
@@ -0,0 +1,15 @@
+package mvnwebapp;
+
+public class Ranjan {
+
+	
+	int a=12;
+	int b=30;
+	int c=40;
+	public void m1(){
+		System.out.println("pravat");
+		System.out.println("Ranjan");
+		System.out.println("Pradhan");
+	}
+	
+}
